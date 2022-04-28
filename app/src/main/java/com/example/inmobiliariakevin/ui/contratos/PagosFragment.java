a840aef1ff74ed015a6eeb11e2caa0bd95321299
KevinPa26 <kevinenriquep26@gmail.com>
2022-04-28 01:38:11 -0300

    Vistas de pagos para los contratos

--- app/src/main/java/com/example/inmobiliariakevin/ui/contratos/PagosFragment.java
+++ app/src/main/java/com/example/inmobiliariakevin/ui/contratos/PagosFragment.java
@@ -1,22 +1,32 @@
 package com.example.inmobiliariakevin.ui.contratos;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.inmobiliariakevin.R;
+import com.example.inmobiliariakevin.modelo.Pago;
+
+import java.util.ArrayList;
 
 public class PagosFragment extends Fragment {
 
-    private PagosViewModel mViewModel;
+    private RecyclerView rvPagosContrato;
+    private PagosAdapter adapter;
+    private PagosViewModel pagosViewModel;
+    private Context context;
 
     public static PagosFragment newInstance() {
         return new PagosFragment();
@@ -25,14 +35,26 @@ public class PagosFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pagos_fragment, container, false);
+
+        View root = inflater.inflate(R.layout.pagos_fragment, container, false);
+        context = root.getContext();
+        inicializar(root);
+        return root;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PagosViewModel.class);
-        // TODO: Use the ViewModel
+    private void inicializar(View root) {
+        rvPagosContrato = root.findViewById(R.id.rvPagosContrato);
+        pagosViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(PagosViewModel.class);
+        pagosViewModel.getPagos().observe(getViewLifecycleOwner(), new Observer<ArrayList<Pago>>() {
+            @Override
+            public void onChanged(ArrayList<Pago> pagos) {
+                GridLayoutManager gridLayoutManager = new GridLayoutManager(context,1,GridLayoutManager.VERTICAL,false);
+                rvPagosContrato.setLayoutManager(gridLayoutManager);
+                adapter = new PagosAdapter(context,pagos,getLayoutInflater());
+                rvPagosContrato.setAdapter(adapter);
+            }
+        });
+        pagosViewModel.cargarPagosDeContratos(getArguments());
     }
 
 }
--- app/src/main/java/com/example/inmobiliariakevin/ui/contratos/PagosViewModel.java
+++ app/src/main/java/com/example/inmobiliariakevin/ui/contratos/PagosViewModel.java
@@ -1,7 +1,34 @@
 package com.example.inmobiliariakevin.ui.contratos;
 
+import android.os.Bundle;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.inmobiliariakevin.Request.ApiClient;
+import com.example.inmobiliariakevin.modelo.Contrato;
+import com.example.inmobiliariakevin.modelo.Pago;
+
+import java.util.ArrayList;
+
 public class PagosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private MutableLiveData<ArrayList<Pago>> pagos;
+    public PagosViewModel() {
+        super();
+    }
+
+    public LiveData<ArrayList<Pago>> getPagos() {
+        if (pagos == null) {
+            pagos = new MutableLiveData<>();
+        }
+        return pagos;
+    }
+
+    public void cargarPagosDeContratos(Bundle bundle) {
+        Contrato pagosLista  = (Contrato) bundle.getSerializable("contrato");
+        ApiClient api = ApiClient.getApi();
+        this.pagos.setValue(api.obtenerPagos(pagosLista));
+    }
 }
